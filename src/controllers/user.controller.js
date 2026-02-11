3485c9bc0775834b7811e8c29edb6e381bf01fef
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-02-11 16:30:00 +0530

    Added WatchHistory Endpoint in User Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -471,7 +471,64 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
 
 });
 
+//second nested aggregation pipeline to get watchhistory
+const getWatchHistory = asyncHandler(async(req, res)=>{
 
+    const user = await User.aggregate([
+        {
+            $match: {
+                // mongoose does not work and  the entire code of the aggregaiton pipeline goes directly
+                //so we have to intentionally call the moongoose method
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchhistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch History fetched successfully"
+        )
+    )
+
+});
 export {
     registerUser,
     loginUser,
