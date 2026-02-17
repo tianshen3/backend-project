b8e4c04c38fcaf538ece7237b7c91e749b8becfd
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-02-17 20:14:14 +0530

    some minor issues

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,7 @@ const ConnectDB = async () => {
             console.log(`\n MongoDB connected ! ! DB Host : ${connectionInstance.connection.host}`);
     } catch(error) {
         console.log("MONGODB connection error ", error);
-        process.exit(1)
+        process.exit(1);
     }
 }
     
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,8 @@ import {
     refreshAccessToken, 
     updateUserAvatar, 
     getUserChannelProfile, 
-    getWatchHistory 
+    getWatchHistory, 
+    updateUserCoverImage
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
